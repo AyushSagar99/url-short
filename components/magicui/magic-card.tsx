ca1c085f7bedd0a539c6bc83bc5534bcdf6686e6
Ayush Sagar <saggar.ayush@gmail.com>
2025-02-27 12:20:10 +0530

    ui

--- components/magicui/magic-card.tsx
+++ components/magicui/magic-card.tsx
@@ -17,7 +17,7 @@ export function MagicCard({
   children,
   className,
   gradientSize = 200,
-  gradientColor = "#262626",
+  gradientColor = "#D9D9D955",
   gradientOpacity = 0.8,
   gradientFrom = "#9E7AFF",
   gradientTo = "#FE8BBB",
